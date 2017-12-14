b912df0c4f4b5fed49f43a4a317b116699343e6f
Julien <julien.fleckinger@laposte.net>
2017-12-14 23:47:31 +0100

    first commit

---
+++ Assets/Scripts/AnimScripts/IdleToRun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class IdleToRun : MonoBehaviour {
+    
+    Animator myAnimator;
+    CharacterController cController;
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        cController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float tmp = 2 - Input.GetAxis("Sprint");
+        
+        myAnimator.SetFloat("vSpeed", Input.GetAxis("Vertical") / tmp);
+        myAnimator.SetFloat("hSpeed", Input.GetAxis("Horizontal") / tmp);
+        myAnimator.SetBool("Grounded", cController.isGrounded);
+
+    }
+}
---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+    public float maxSpeed = 5.0f;
+    public float rotationSpeed = 0.3f;
+    public float jumpSpeed = 5;
+    public LayerMask groundLayer;
+    public float gravity = 9;
+    public bool grounded;
+    private Vector3 curSpeed;
+    private CharacterController cController;
+    Vector3 input = new Vector3(0, 0, 1);
+    private Vector3 moveDirection;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        cController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = cController.isGrounded;
+        //transform.position += input * speed * Time.deltaTime;
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        float tmp = 2 - Input.GetAxis("Sprint");
+
+        transform.Rotate(Vector3.up, 10.0f * mouseInput.x * rotationSpeed);
+
+        if (cController.isGrounded){
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= (maxSpeed / tmp);
+            if (Input.GetAxisRaw("Jump") == 1)
+                moveDirection.y = jumpSpeed;
+
+        }
+        else
+            moveDirection.y -= gravity * Time.deltaTime;
+        cController.Move(moveDirection * Time.deltaTime);
+
+    }
+   
+}
