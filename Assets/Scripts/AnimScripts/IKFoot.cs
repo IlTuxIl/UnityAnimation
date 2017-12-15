66b8cd47cfbe3bca02107df7c930768c39437a8e
IlTuxIl <julien.fleckinger@laposte.net>
2017-12-15 17:08:20 +0100

    htrdh

--- Assets/Scripts/AnimScripts/IKFoot.cs
+++ Assets/Scripts/AnimScripts/IKFoot.cs
@@ -1,25 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Net.Sockets;
 
 [RequireComponent(typeof(Animator))]
 
 public class IKFoot : MonoBehaviour {
 	Animator cController;
-	public Transform leftPos;
-	public Transform rightPos;
-	// Use this for initialization
+	public Vector3 leftPos;
+	public Vector3 rightPos;
+
+	public Quaternion leftNorm;
+	public Quaternion RightNorm;
+
+	public Vector3 offset = new Vector3(0.5f,0f,0f);
+ 	// Use this for initialization
 	void Start () {
 		cController = GetComponent<Animator> ();
 	}
-	
+
+	void Update(){
+		RaycastHit hitLeft;
+		RaycastHit hitRight;
+
+		if (Physics.Raycast (transform.position - offset, -Vector3.up, out hitLeft)) {
+			leftPos = hitLeft.point;
+			leftNorm = hitLeft.transform.rotation;
+		}
+		
+		if (Physics.Raycast (transform.position + offset, -Vector3.up, out hitRight)) {
+			rightPos = hitRight.point;
+			RightNorm = hitRight.transform.rotation;
+		}
+	}
+
 	// Update is called once per frame
 	void OnAnimatorIK () {
-		cController.SetIKPosition (AvatarIKGoal.LeftFoot, leftPos.position);
-		cController.SetIKPosition (AvatarIKGoal.RightFoot, rightPos.position);
+		cController.SetIKPosition (AvatarIKGoal.LeftFoot, leftPos);
+		cController.SetIKPosition (AvatarIKGoal.RightFoot, rightPos);
 
 		cController.SetIKPositionWeight (AvatarIKGoal.LeftFoot, 1);
 		cController.SetIKPositionWeight (AvatarIKGoal.RightFoot, 1);
+
+		cController.SetIKRotation (AvatarIKGoal.LeftFoot, leftNorm);
+		cController.SetIKRotation (AvatarIKGoal.RightFoot, RightNorm);
+
+		cController.SetIKRotationWeight (AvatarIKGoal.LeftFoot, 1);
+		cController.SetIKRotationWeight (AvatarIKGoal.RightFoot, 1);
 	}
 }
